d23952b9959e3eb24c718a1283d00baf4cd6bd1a
Ian Burrett <572883+weestoater@users.noreply.github.com>
2025-03-06 21:44:58 +0000

    archived utils class item

---
+++ src/content/react/utils.js
@@ -0,0 +1,18 @@
+(function () {
+  // set up the utils class
+  var utils = {
+    /**
+     * Runs a test
+     * @function
+     */
+    test: function () {
+      /**
+       * Logs a message to the console
+       */
+      console.log("This is a test for the IIFE");
+    },
+  };
+
+  // execute the test()
+  utils.test();
+})();
--- src/pages/React.tsx
+++ src/pages/React.tsx
@@ -3,16 +3,18 @@ import { ReactHooksPost } from "../content/react/ReactHooks";
 import { NextThingPost } from "../content/react/NextThingPost";
 import { SaltPost } from "../content/react/SaltPost";
 import { ViteContent } from "../content/react/ViteReact";
+import { UtilsScript } from "../content/react/UtilsScript";
 
 export const ReactPage = () => {
   return (
     <>
-      <PageTitleH1 title="React articles" />
+      <PageTitleH1 title="React & JS articles" />
       <div className="grid-auto-fill">
         <ViteContent />
         <SaltPost />
         <NextThingPost />
         <ReactHooksPost />
+        <UtilsScript />
       </div>
     </>
   );
