24148f73da1e0d31b9f398757b0947d7a8a3d746
Ian Burrett <572883+weestoater@users.noreply.github.com>
2025-05-24 15:53:15 +0100

    added Ross County result and archived the previous years

--- src/pages/Football.tsx
+++ src/pages/Football.tsx
@@ -4,18 +4,12 @@ import { Nav, NavItem, NavLink, TabContent, TabPane } from "reactstrap";
 import { PageTitleH1 } from "../components/global/pageTitleHeading";
 import { MatchDetails } from "../components/football/matchDetails";
 import { GoalScorerDetails } from "../components/football/goalScorerDetails";
-import mfcGoals from "../data/mfc-goals.json";
-import mfcMatches from "../data/mfc-matches.json";
+import mfcGoals from "../data/2024-25-goals.json";
+import mfcMatches from "../data/2024-25-matches.json";
 import { FootballIntro } from "../content/football/footballIntro";
 
 export const FootballPage = () => {
-  const seasons = [
-    "2024-2025",
-    "2023-2024",
-    "2022-2023",
-    "2021-2022",
-    "2020-2021",
-  ];
+  const seasons = ["2024-2025"];
 
   const [activeTab, setActiveTab] = useState(seasons[0]);
 
